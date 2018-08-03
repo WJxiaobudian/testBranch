eead8456452ad34e9e11f9fabee440ee76fbb464
王洁（万数科技） <13283817653@163.com>
2018-08-03 17:27:33 +0800

    1.2.5

--- testBranch/testBranch/F_ADD_!ViewController.m
+++ testBranch/testBranch/F_ADD_!ViewController.m
@@ -18,23 +18,23 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    UILabel *label = [[UILabel alloc] init];
-    [self.view addSubview:label];
-    
-    UILabel *label2 = [[UILabel alloc] init];
-    [self.view addSubview:label2];
-    UILabel *label3 = [[UILabel alloc] init];
-    [self.view addSubview:label3];
-    UILabel *label4 = [[UILabel alloc] init];
-    [self.view addSubview:label4];
-    UILabel *label5 = [[UILabel alloc] init];
-    [self.view addSubview:label5];
-    UILabel *label6 = [[UILabel alloc] init];
-    [self.view addSubview:label6];
-    UILabel *label7 = [[UILabel alloc] init];
-    [self.view addSubview:label7];
-    UILabel *label8 = [[UILabel alloc] init];
-    [self.view addSubview:label8];
+//    UILabel *label = [[UILabel alloc] init];
+//    [self.view addSubview:label];
+//    
+//    UILabel *label2 = [[UILabel alloc] init];
+//    [self.view addSubview:label2];
+//    UILabel *label3 = [[UILabel alloc] init];
+//    [self.view addSubview:label3];
+//    UILabel *label4 = [[UILabel alloc] init];
+//    [self.view addSubview:label4];
+//    UILabel *label5 = [[UILabel alloc] init];
+//    [self.view addSubview:label5];
+//    UILabel *label6 = [[UILabel alloc] init];
+//    [self.view addSubview:label6];
+//    UILabel *label7 = [[UILabel alloc] init];
+//    [self.view addSubview:label7];
+//    UILabel *label8 = [[UILabel alloc] init];
+//    [self.view addSubview:label8];
     
     
 //    UILabel *label2 = [[UILabel alloc] init];

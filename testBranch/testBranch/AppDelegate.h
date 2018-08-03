88147ad27f97637dc9def3ec7faa5182be564a49
王洁（万数科技） <13283817653@163.com>
2018-08-03 16:40:43 +0800

    导入工程

---
+++ testBranch/testBranch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testBranch
+//
+//  Created by 王洁 on 2018/8/3.
+//  Copyright © 2018年 王洁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
